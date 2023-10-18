5e7bfa188f3dd24956713ee342e757f8c18ae967
Andrew <andrewhubbard78@gmail.com>
2023-10-18 12:30:30 -0400

    A

--- app.js
+++ app.js
@@ -3,8 +3,28 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+const firstName = "Frodo";
+let lastName = "Baggins";
+var age = 51;
 
-// Exercise 2
+lastName = "Bilbo";
+age = 52;
 
-// Exercise 3
+let language = "Javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log('${language} was first release in ${createdyear}.');
+
+let product = "T-shirty";
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let instock= false;
+let selectedSize = "M";
+
+console.log(
+  '${product} in size ${selectedSize} is $${price} with ${stock}
+  in stock.' 
+);
